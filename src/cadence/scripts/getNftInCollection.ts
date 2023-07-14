5acc4825c03e77cf1f0adff1da7c2ae810fcecd1
a0p0bhv <akshay.prakash@walmart.com>
2023-07-14 18:52:41 +0530

    Add NFT Scripts

--- src/cadence/scripts/getNftInCollection.ts
+++ src/cadence/scripts/getNftInCollection.ts
@@ -1,7 +1,7 @@
 import { executeScript } from '@onflow/flow-cadut';
 import { getContractAddresses, getScript, ScriptName } from '../raw';
 
-export const getNftInCollection = async () => {
+export const getNftInCollection = async (address: string, publicPath: string) => {
 //   // Get the message's transaction code
   const code = getScript(ScriptName.GET_NFT_IN_COLLECTION);
 
@@ -12,8 +12,9 @@ export const getNftInCollection = async () => {
   const [result, err] = await executeScript({
     code: code,
     addressMap: contractAddresses,
+    args: [address, publicPath]
   });
-  
+
   console.log(err)
   return result
 };
